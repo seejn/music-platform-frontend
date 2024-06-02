6dbdde799b599bb820920266fd5787422a83df8f
seejn <seejnmaharjan@gmail.com>
2024-06-02 22:16:25 +0545

    created pages for artist
    pages:
    - Home
    - Browse
    - Library
    - Profile
    - Uploads
    - Settings

---
+++ src/pages/Artist/Library.vue
@@ -0,0 +1,23 @@
+<template>
+    <Layout>
+        <template #Main>
+            <section>
+                <h2 class="text-2xl font-bold my-4">My Musics</h2>
+                <TrackCollection />
+            </section>
+            <section class="py-4">
+                <h2 class="text-2xl font-bold my-4">My Albums</h2>
+                <AlbumCollection />
+            </section>
+            <section class="py-4">
+                <h2 class="text-2xl font-bold my-4">Popular Songs</h2>
+                <TrackCollection />
+            </section>
+        </template>
+    </Layout>
+</template>
+
+<script setup>
+import AlbumCollection from '../../components/Album/AlbumCollection.vue'
+import TrackCollection from '../../components/Track/TrackCollection.vue'
+</script>
