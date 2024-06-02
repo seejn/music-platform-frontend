7edacadfd1c073438312072439a68f08dc71615d
seejn <seejnmaharjan@gmail.com>
2024-06-02 22:13:32 +0545

    created navbar, sidebar, footer and layout
    layout contains navbar, sidebar, footer
    - common component over multiple pages in single layout component
    - for the purpose of utilizing same layout over multiple pages
    - layout reusability

---
+++ src/components/Header/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="h-full bg-white shadow">
+    <div class="container mx-auto px-4 py-3 flex justify-between items-center">
+      <div class="text-2xl font-bold">MusicPlatform</div>
+      <nav class="space-x-4">
+        <RouterLink v-for="route in navbarRoutes" :to="route.path" class="text-gray-700 hover:text-gray-900">{{ route.name }}</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup>
+  import { navbarRoutes } from '../../router';
+</script>
---
+++ src/pages/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="h-screen overflow-hidden">
+        <div class="w-full" style="height: 8%;">
+            <Navbar />
+        </div>
+        <div class="w-full h-full  flex" style="height: 92%;">
+            <div class="w-80 h-full overflow-y-auto">
+                <Sidebar />
+            </div>
+            <div class="w-full h-full overflow-y-auto container mx-auto px-4 pt-4">
+                <slot name="Main" />
+                <Footer />
+            </div>
+        </div>
+    </section>
+</template>
