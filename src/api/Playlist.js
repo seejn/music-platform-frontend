a363d5938785eff018af78cb6ad01eb1df2361e5
SaloniStha02 <saloni.stha2002@gmail.com>
2024-06-03 17:48:46 +0545

    Created fetch all mmethod

---
+++ src/api/Playlist.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export const fetchAllPlaylists = async () => {
+    const url = `${import.meta.env.VITE_API_BASE_URL}/track/get_all_playlist/`
+    try{
+        const response = await axios(url , {
+            method: 'get',
+        })
+        return response.data.data
+    }catch(error){
+        console.log(error)
+        return error
+    }
+}
