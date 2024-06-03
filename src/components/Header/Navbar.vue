281389ab41947306b6d4387c69ef4575ee3c527c
seejn <seejnmaharjan@gmail.com>
2024-06-03 10:06:54 +0545

    added active class

--- src/components/Header/Navbar.vue
+++ src/components/Header/Navbar.vue
@@ -3,12 +3,20 @@
     <div class="container mx-auto px-4 py-3 flex justify-between items-center">
       <div class="text-2xl font-bold">MusicPlatform</div>
       <nav class="space-x-4">
-        <RouterLink v-for="route in navbarRoutes" :to="route.path" class="text-gray-700 hover:text-gray-900">{{ route.name }}</RouterLink>
+        <RouterLink v-for="route in routes" :to="route.path" class="text-gray-700 hover:text-gray-900">{{ route.name }}</RouterLink>
       </nav>
     </div>
   </header>
 </template>
 
 <script setup>
-  import { navbarRoutes } from '../../router';
+  import { routes as routes } from '../../router';
 </script>
+
+<style>
+  nav .router-link-active{
+    color: rgb(17 24 39);
+    padding-bottom: 0.5rem;
+    border-bottom: 3px solid rgb(31 41 55);
+  }
+</style>
