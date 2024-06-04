71dbc15701f716642597ac9a0ccc902fffa4ae47
dyo78 <maharjan.diyaa@gmail.com>
2024-06-04 21:28:54 +0545

    authentication api

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -11,6 +11,7 @@ export const createUser = async (data) => {
   }
 };
 
+
 export const createArtist = async (data) => {
   const url = `${import.meta.env.VITE_API_BASE_URL}/roles/create_artists/`;
   try {
@@ -22,10 +23,16 @@ export const createArtist = async (data) => {
   }
 };
 
+
 export const Login = async (data) => {
   const url = `${import.meta.env.VITE_API_BASE_URL}/roles/login/`;
   try {
     const response = await axios.post(url, data);
+
+    localStorage.setItem('access_token', response.data.access);
+    localStorage.setItem('refresh_token', response.data.refresh);
+    localStorage.setItem('user', JSON.stringify(response.data.user));
+
     return response.data.data;
   } catch (error) {
     console.error(error);
--- src/pages/Guest/SignUp.vue
+++ src/pages/Guest/SignUp.vue
@@ -11,19 +11,39 @@
             <div>
               <h2 class="text-3xl font-bold mb-8 text-black text-center">Registration Form</h2>
               <div class="space-y-6">
-                <div v-for="(field, index) in formFields" :key="index" class="form-group">
+                <div v-for="(field, index) in fields" :key="index" class="form-group">
                   <label :for="field.id" :class="labelClasses">{{ field.label }}</label>
-                  <component
-                    :is="field.type === 'select' ? 'select' : 'input'"
+                  <input
                     v-model="info[field.model]"
                     :id="field.id"
-                    :class="inputClasses"
                     :type="field.type"
+                    :class="inputClasses"
                     :required="field.required"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="dob" :class="labelClasses">Date of Birth</label>
+                  <input
+                    v-model="info.dob"
+                    id="dob"
+                    :class="inputClasses"
+                    type="date"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="gender" :class="labelClasses">Gender</label>
+                  <select
+                    v-model="info.gender"
+                    id="gender"
+                    :class="inputClasses"
+                    required
                   >
-                    <option v-if="field.type === 'select'" disabled value="">Select {{ field.label }}</option>
-                    <option v-for="option in field.options" :key="option.value" :value="option.value">{{ option.text }}</option>
-                  </component>
+                    <option disabled value="">Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
                 </div>
                 <div class="form-submit flex justify-center mt-8">
                   <button type="submit" class="bg-white border-blood border-2 hover:bg-blood hover:text-white text-black font-bold py-2 px-4 rounded-full mb">Submit</button>
@@ -45,66 +65,51 @@
 
 <script>
 import { ref } from 'vue';
-import axios from 'axios';
 import GuestNav from '../../components/Header/GuestNav.vue';
 import { useRouter } from 'vue-router';
+import { createUser } from '../../api/Auth';
 
 export default {
-  name: 'Login',
+  name: 'Register',
   components: {
     GuestNav,
   },
   setup() {
+    const router = useRouter();
+    
     const info = ref({
       id: null,
-      first_name: 'John',
-      last_name: 'Doe',
-      dob: '1990-01-01',
-      gender: 'M',
-      address: '123 Main St, Anytown, USA',
-      email: 'john.doe@example.com',
-      password: 'password123',
+      first_name: '',
+      last_name: '',
+      dob: '',
+      gender: '',
+      email: '',
+      password: '',
     });
 
-    const formFields = [
-      { label: 'First Name', id: 'first_name', type: 'text', model: 'first_name', required: true },
-      { label: 'Last Name', id: 'last_name', type: 'text', model: 'last_name', required: true },
-      { label: 'Date of Birth', id: 'dob', type: 'date', model: 'dob', required: false },
-      { label: 'Email', id: 'email', type: 'email', model: 'email', required: true },
-      { label: 'Gender', id: 'gender', type: 'select', model: 'gender', required: false, options: [
-          { text: 'Male', value: 'M' },
-          { text: 'Female', value: 'F' },
-          { text: 'Other', value: 'O' },
-        ],
-      },
-      { label: 'Address', id: 'address', type: 'text', model: 'address', required: false },
-      { label: 'Password', id: 'password', type: 'password', model: 'password', required: true },
+    const fields = [
+      { id: 'first_name', label: 'First Name', model: 'first_name', type: 'text', required: true },
+      { id: 'last_name', label: 'Last Name', model: 'last_name', type: 'text', required: true },
+      { id: 'email', label: 'Email', model: 'email', type: 'email', required: true },
+      { id: 'password', label: 'Password', model: 'password', type: 'password', required: true },
     ];
 
     const labelClasses = 'block text-sm font-medium text-gray-700 mb-2';
     const inputClasses = 'form-input w-full rounded-md border border-blood bg-white text-gray-900 p-2';
 
-    const submitForm = () => {
-      console.log('Form submitted with the following data:', info.value);
-      // Dummy data submission
-      // Comment out axios.post and toast lines for dummy data
-      // axios.post('http://127.0.0.1:8000/ams/artists/', info.value)
-      //   .then(response => {
-      //     if (response.status === 201) {
-      //       clearForm();
-      //       toast.success("User created successfully");
-      //     }
-      //   })
-      //   .catch(error => {
-      //     toast.error("User not created");
-      //   });
+    const submitForm = async () => {
+      try {
+        const response = await createUser(info.value);
+        console.log('User created successfully:', response);
+        clearForm();
+        // Implement success toast or message here
+      } catch (error) {
+        console.error('User creation failed:', error);
+        // Implement error toast or message here
+      }
     };
 
-    const router = useRouter();
-
     const navigateToLogin = () => {
-      console.log('Navigate to login');
-      // Dummy navigation
       router.push('/login');
     };
 
@@ -115,7 +120,6 @@ export default {
         last_name: '',
         dob: '',
         gender: '',
-        address: '',
         email: '',
         password: '',
       };
@@ -123,7 +127,7 @@ export default {
 
     return {
       info,
-      formFields,
+      fields,
       labelClasses,
       inputClasses,
       submitForm,
