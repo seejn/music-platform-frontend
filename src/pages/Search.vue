c36a6c728105f99a9f7523138afa239b52d0ad17
dyo78 <maharjan.diyaa@gmail.com>
2024-06-04 12:00:26 +0545

    moved from temp to src

---
+++ src/pages/Search.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="p-6">
+    <input
+      v-model="searchQuery"
+      placeholder="Search for albums or artists..."
+      class="w-full p-4 mb-6 rounded-lg  bg-black text-white"
+    />
+    <div v-if="filteredResults.length" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div
+        v-for="result in filteredResults"
+        :key="result"
+        class="bg-black p-6 rounded-lg text-center text-white"
+      >
+        {{ result }}
+      </div>
+    </div>
+    <div v-else class="text-center text-white">
+      No results found.
+    </div>
+    <TopArtist/>
+  </div>
+</template>
+
+<script>
+import TopArtist from '../../saloni/components/User/TopArtist.vue';
+export default {
+  components:{
+    TopArtist
+  },
+  data() {
+    return {
+      searchQuery: '',
+      mockData: [
+        'Billie',
+        'Taylor',
+        'No time to die',
+        'Watermelon Sugar',
+        'Sunflower',
+        'Sunflower-post',
+        'Daylight',
+        'Dua Lipa'
+      ]
+    };
+  },
+  computed: {
+    filteredResults() {
+      if (this.searchQuery.trim() === '') {
+        return [];
+      }
+      return this.mockData.filter(item =>
+        item.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  }
+};
+</script>
