c90ff99174a22960ff9d01737a4977651f39a7db
SaloniStha02 <saloni.stha2002@gmail.com>
2024-06-06 13:08:45 +0545

    button margin added

--- src/components/Artist/ArtistCard.vue
+++ src/components/Artist/ArtistCard.vue
@@ -12,18 +12,22 @@
 
 <template>
     <div class="artist flex-shrink-0 mr-10">
-        <img src="/src/assets/pic/ha.jpeg" alt="Cover Image" class="w-60 h-60 rounded-full">
+        <img :src="imagesUrl" alt="Cover Image" class="w-60 h-60 rounded-full">
         <h3 class="text-xl text-white mt-3 text-center">{{ artist?.first_name }}</h3>
     </div>
 
 </template>
 
 <script setup>
-
+import { computed } from 'vue';
 const props = defineProps({
 artist: {
     type: Object,
     required: true
 }
 })
+const imagesUrl =  computed(() => {
+    if(props.artist.image)    return `${import.meta.env.VITE_API_BASE_URL}${props.artist.image}`;
+    else return "/src/assets/pic/blond.jpeg"
+});
 </script>
