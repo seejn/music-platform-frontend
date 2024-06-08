4d5b9df1282dc2fd114e5f01b17aca274362c83f
SaloniStha02 <saloni.stha2002@gmail.com>
2024-06-08 11:26:41 +0545

    landing page made

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,7 +1,14 @@
 <template>
-    <Layout>
-        <template #Main>
-            <h1 class="text-green-500 text-5xl">LandingPage</h1>
-        </template>
-    </Layout>
+  <Layout>
+    <template #Main>
+      <div class="relative w-full h-full">
+        <img src="/src/assets/pic/banner.png" alt="Banner" class="w-full h-full object-cover">
+        <div class="absolute inset-0 flex items-start justify-start top-56">
+          <h6 class="text-white text-4xl md:text-6xl lg:text-7xl bg-black bg-opacity-50 p-4 rounded">
+            Listen to your favorite Artist, <span class="text-red-800">Anytime,</span> Anywhere
+          </h6>
+        </div>
+      </div>
+    </template>
+  </Layout>
 </template>
