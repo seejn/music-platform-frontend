518030f31ca503d0b075dc998b3e127824630e0f
seejn <seejnmaharjan@gmail.com>
2024-06-09 23:51:24 +0545

    minor fix in LandingLayout

--- src/pages/LandingLayout.vue
+++ src/pages/LandingLayout.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="w-full h-full" style="height: 92%;">
             
-            <div class="bg-black w-full h-full flex flex-col justify-between overflow-y-auto container">
+            <div class="bg-black w-full h-full flex flex-col justify-between">
                 <div class="px-4 pt-4 w-full">
                     <slot name="Main" />
                 </div>
