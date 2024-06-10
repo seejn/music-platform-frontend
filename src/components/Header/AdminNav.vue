5b82143370d0d76b1832132aef35dc5f67bb0468
dyo78 <maharjan.diyaa@gmail.com>
2024-06-10 22:42:55 +0545

    fixed api

--- src/components/Header/AdminNav.vue
+++ src/components/Header/AdminNav.vue
@@ -5,7 +5,7 @@
         <div class="absolute left-0 p-6 text-3xl font-semibold">
           Artist Management System
         </div>
-        <ul class="absolute right-0 flex space-x-4 p-6">
+        <!-- <ul class="absolute right-0 flex space-x-4 p-6">
           <RouterLink to="/admin/dashboard" class="p-5 font-bold text-lg hover:text-red-800">
             Dashboard
           </RouterLink>
@@ -20,9 +20,12 @@
           </RouterLink>
           <RouterLink to="/admin/banned-songs" class="p-5 font-bold text-lg hover:text-red-800">
             Banned Songs </RouterLink>
-        </ul>
+        </ul> -->
       </div>
       <ul class="absolute right-0 flex space-x-4 p-6">
+        <RouterLink to="/admin/dashboard/" class="p-5 font-bold text-lg hover:text-orange-800">
+          Dashboard
+        </RouterLink>
         <RouterLink to="/admin/artists" class="p-5 font-bold text-lg hover:text-orange-800">
           Artist
         </RouterLink>
