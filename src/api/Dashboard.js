97de64561a1643dd1084a377e7397bf6a0508515
SaloniStha02 <saloni.stha2002@gmail.com>
2024-06-10 21:41:27 +0545

    Dashboard api made

---
+++ src/api/Dashboard.js
@@ -0,0 +1,53 @@
+import axios from './accessTokenAxios'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+
+export const fetchAllArtistsSongPlaylistCounts = async () => {
+    const url = `${API_BASE_URL}/stats/artists/songs/playlist-counts/`;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const fetchAllArtistsAlbumFavorites = async () => {
+    const url = `${API_BASE_URL}/stats/artists/album/favorites`;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const fetchArtistAlbumCounts = async () => {
+    const url = `${API_BASE_URL}/stats/artists/albums/counts/`;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const fetchTotalArtists = async () => {
+    const url = `${API_BASE_URL}/stats/artists/total/`;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const fetchTotalUsers = async () => {
+    const url = `${API_BASE_URL}/stats/users/total/`;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
