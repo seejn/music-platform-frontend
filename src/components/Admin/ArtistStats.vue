ac09c7b12b648593290d3649e6ed8462b361f6b5
SaloniStha02 <saloni.stha2002@gmail.com>
2024-06-11 13:35:00 +0545

    particular artist stats

--- src/components/Admin/ArtistStats.vue
+++ src/components/Admin/ArtistStats.vue
@@ -1,30 +1,59 @@
 <template>
-    <LandingLayout>
-        <template #Main>
-            <h1 class="text-white">Analytics of Artist {{ id }}</h1>
+<AdminLayout>
+    <template #Main>
+        <h1 class="text-white text-2xl ">Analytics of Artist {{ id }}</h1>
+        <div class="flex justify-between">
 
-            <span v-if="dataLoaded">
-                <Pie :data="albumFavouriteData" :options="chartOptionsForPie" />
-            </span>
+            <div class="flex-2 mr-4">
 
-            <span v-if="dataLoaded">
-                <Bar :data="albumMaleFemaleRatioData" :options="chartOptionsForBar" />
-            </span>
+                <div class=" mt-10">
+                    <span>
+                        <AdminCard :totalAlbums="totalAlbums" :totalTracks="totalTracks" /></span>
+                </div>
+                <div class="bg-zinc-700 mx-10 my-10 h-2/4 rounded-lg">
 
+                    <h6 class="text-white text-2xl mx-10 mt-10">Favorite album based on number of user (gender wise) </h6>
+                    <span v-if="dataLoaded">
+                        <Bar :data="albumMaleFemaleRatioData" :options="chartOptionsForBar" class="w-48 h-36" />
+                    </span>
 
-        </template>
-    </LandingLayout>
+                </div>
+            </div>
+
+            <div class="flex-1 bg-zinc-700 mx-10 my-10 rounded-lg">
+
+                <span v-if="dataLoaded">
+                    <h6 class="text-white text-2xl mx-10 mt-10">Number of favorite songs in an album</h6>
+                    <Pie :data="albumFavouriteData" :options="chartOptionsForPie" />
+                </span>
+            </div>
+        </div>
+    </template>
+</AdminLayout>
 </template>
 
 <script setup>
+import AdminCard from '../Dashboard/AdminCard.vue'
 
-import { onMounted, ref } from 'vue'
-import { Bar, Pie } from 'vue-chartjs'
+import {
+    onMounted,
+    ref
+} from 'vue'
+import {
+    Bar,
+    Pie
+} from 'vue-chartjs'
 
-import { fetchArtistAlbumFavouriteStates } from '../../api/Dashboard.js'
+import {
+    fetchArtistAlbumFavouriteStates
+} from '../../api/Dashboard.js'
 
 import LandingLayout from '../../pages/LandingLayout.vue'
-
+import AdminLayout from '../../pages/AdminLayout.vue';
+import {
+    fetchArtistTotalTracks,
+    fetchArtistTotalAlbums
+} from '../../api/Dashboard.js';
 
 const props = defineProps({
     id: {
@@ -34,102 +63,103 @@ const props = defineProps({
 })
 
 const dataLoaded = ref(false)
-
+const totalAlbums = ref(0)
+const totalTracks = ref(0)
 const artistAlbums = ref([])
 const favouriteCount = ref([])
 
 const albumMaleFemaleRatioData = ref({
     labels: [],
-    datasets: [
-      {
-        label: 'Male',
-        data: [],
-        backgroundColor: 'rgba(75, 192, 192, 0.5)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1
-      },
-      {
-        label: 'Female',
-        data: [],
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1
-      }
+    datasets: [{
+            label: 'Male',
+            data: [],
+            backgroundColor: 'rgba(75, 192, 192, 0.5)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Female',
+            data: [],
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }
     ]
-  })
-  
-  const chartOptionsForBar = ref({
+})
+
+const chartOptionsForBar = ref({
     responsive: true,
     maintainAspectRatio: false,
     scales: {
-      y: {
-        beginAtZero: true,
-        ticks: {
-          color: '#ffffff'
-        }
-      },
-      x: {
-        ticks: {
-          color: '#ffffff'
-        }
-      }
-    },
-    plugins: {
-      legend: {
-        labels: {
-          color: '#ffffff'
-        }
-      },
-      tooltip: {
-        titleColor: '#ffffff',
-        bodyColor: ''
-      }
-    }
-  })
-
-
-  const albumFavouriteData = ref({
-      labels: [],
-      datasets: [
-        {
-          label: 'Number of Plays',
-          data: [],
-          backgroundColor: ["#dedad2", "#e4bcad", "#df979e", "#d7658b", "#c80064"],
-          borderColor: '#ffffff',
-          borderWidth: 1
-        }
-      ]
-    })
-
-    const chartOptionsForPie = ref({
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
         y: {
-          beginAtZero: true,
-          ticks: {
-            color: '#ffffff'
-          }
+            beginAtZero: true,
+            ticks: {
+                color: '#ffffff'
+            }
         },
         x: {
-          ticks: {
-            color: '#ffffff'
-          }
+            ticks: {
+                color: '#ffffff'
+            }
         }
-      },
-      plugins: {
+    },
+    plugins: {
         legend: {
-          labels: {
-            color: '#ffffff'
-          }
+            labels: {
+                color: '#ffffff'
+            }
         },
         tooltip: {
-          titleColor: '#ffffff',
-          bodyColor: '#ffffff'
+            titleColor: '#ffffff',
+            bodyColor: ''
         }
-      }
-    })
+    },
+    elements: {
+        arc: {
+            borderColor: '#ffffff'
+        }
+    }
+})
 
+const albumFavouriteData = ref({
+    labels: [],
+    datasets: [{
+        label: 'Number of Plays',
+        data: [],
+        backgroundColor: ["#dedad2", "#e4bcad", "#df979e", "#d7658b", "#c80064"],
+        borderColor: '#ffffff',
+        borderWidth: 1
+    }]
+})
+
+const chartOptionsForPie = ref({
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+        y: {
+            beginAtZero: true,
+            ticks: {
+                color: '#ffffff'
+            }
+        },
+        x: {
+            ticks: {
+                color: '#ffffff'
+            }
+        }
+    },
+    plugins: {
+        legend: {
+            labels: {
+                color: '#ffffff'
+            }
+        },
+        tooltip: {
+            titleColor: '#ffffff',
+            bodyColor: '#ffffff'
+        }
+    }
+})
 
 const loadArtistAlbumFavouriteStats = async () => {
     const response = await fetchArtistAlbumFavouriteStates(props.id)
@@ -141,18 +171,18 @@ const loadArtistAlbumFavouriteStats = async () => {
     const favouriteByGender = response.data.map((res) => res.favourites_by_gender)
 
     const favouriteByMale = favouriteByGender.map((res) => {
-       if(res[0] && res[0].user__gender === "male") return res[0].count
-       else return 0
-    })
-    const favouriteByFemale = favouriteByGender.map((res) => {
-        if(res[0] && res[0].user__gender === "female") return res[0].count
+        if (res[0] && res[0].user__gender === "male") return res[0].count
         else return 0
     })
-    
+    const favouriteByFemale = favouriteByGender.map((res) => {
+        if (res[0] && res[0].user__gender === "female") return res[0].count
+        else return 0
+    })
+
     albumMaleFemaleRatioData.value.labels = albums
     albumMaleFemaleRatioData.value.datasets[0].data = favouriteByMale
     albumMaleFemaleRatioData.value.datasets[1].data = favouriteByFemale
-    
+
     albumFavouriteData.value.labels = albums
     albumFavouriteData.value.datasets[0].data = favouriteCount
 
@@ -161,10 +191,17 @@ const loadArtistAlbumFavouriteStats = async () => {
     dataLoaded.value = true
 }
 
+const loadCount = async (id) => {
+    const tracks = await fetchArtistTotalTracks(id);
+    const albums = await fetchArtistTotalAlbums(id);
+    totalTracks.value = tracks.total_tracks
+    totalAlbums.value = albums.total_albums
 
+}
 
 onMounted(() => {
+    loadCount(props.id)
     loadArtistAlbumFavouriteStats()
-})
 
+})
 </script>
