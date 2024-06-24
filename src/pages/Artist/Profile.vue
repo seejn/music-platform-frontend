1957cef202f9239a338cc4ba07466eacc4b6bdc7
seejn <seejnmaharjan@gmail.com>
2024-06-24 17:21:46 +0545

    state changed from user to artist

--- src/pages/Artist/Profile.vue
+++ src/pages/Artist/Profile.vue
@@ -10,7 +10,7 @@
         <div class="p-6 pt-16 bg-black max-h-full flex-grow relative z-1">
           <div class="flex flex-row">
             <div class="relative group">
-              <img :src="getProfileImageUrl(user?.image)" alt="Artist Image"
+              <img :src="getProfileImageUrl(artist?.image)" alt="Artist Image"
                 class="w-60 h-60 border-4 rounded-full border-red-800">
               <div
                 class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer"
@@ -22,7 +22,7 @@
 
 
             <p class="font-bold text-white text-5xl ml-2 mt-[7vw]">
-              {{ user?.first_name }} {{ user?.last_name }}
+              {{ artist?.first_name }} {{ artist?.last_name }}
               <button @click="toggleEditForm"
                 class="border-2 border-red-800 text-white -mt-9 hover:ring-2 hover:ring-red-500 text-xl rounded-lg px-4 py-2">
                 Edit
@@ -227,6 +227,7 @@ const saveImage = async () => {
     console.log("saveImage", response)
 
     user.value = response
+    artist.value = response
     window.localStorage.setItem("user", JSON.stringify(user.value))
     showImageForm.value = false;
     toast.success("Images saved successfully")
@@ -259,6 +260,7 @@ const updateArtistDetails = async (updatedUser) => {
   try {
     const response = await updateArtist(user.value.id,updatedUser);
     user.value = response;
+    artist.value = response;
     toast.success("Profile updated successfully")
     console.log(user.value)
     showEditForm.value = false;
