2919deeb84f323f4e7b7f6a002df0a46329db154
SaloniStha02 <saloni.stha2002@gmail.com>
2024-06-24 10:54:22 +0545

    image edit formcss changed

--- src/pages/Playlist.vue
+++ src/pages/Playlist.vue
@@ -171,13 +171,13 @@
         <transition name="fade">
           <div v-if="showImageForm"
             class="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75">
-            <div class="bg-black p-8 rounded-lg">
+            <div class="bg-black p-8 rounded-lg  border-2 border-red-800">
               <h2 class="text-2xl font-bold mb-4 text-white">Save Image</h2>
 
               <img :src="imageUrl" alt="Selected Image" class="w-60 h-60 border-4 border-blood mb-4">
               <div class="flex justify-end space-x-4">
-                <button @click="saveImage" class="px-4 py-2 bg-gray-300 text-white rounded-md">Save</button>
-                <button @click="cancelImage" class="px-4 py-2 bg-gray-300 text-white rounded-md">Cancel</button>
+                <button @click="saveImage" class="px-4 py-2 bg-gray-300 text-white rounded-md ring-2 ring-red-800 hove:bg-red-800">Save</button>
+                <button @click="cancelImage" class="px-4 py-2 bg-gray-300 text-white rounded-md ring-2 ring-red-800 hove:bg-red-800">Cancel</button>
               </div>
             </div>
           </div>
