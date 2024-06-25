81bfdf02306d9bda749d2071cdf10d4d0cec8ab5
seejn <seejnmaharjan@gmail.com>
2024-06-25 16:32:22 +0545

    changed default image placeholder

--- src/utils/imageUrl.js
+++ src/utils/imageUrl.js
@@ -1,9 +1,9 @@
 export const getImageUrl = (image) => {
   if (props.user?.image) return `${import.meta.env.VITE_API_BASE_URL}${image}`;
-  else return "/src/assets/placeholders/image.png";
+  else return "https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg";
 };
 
 export const getProfileImageUrl = (image) => {
   if (image) return `${import.meta.env.VITE_API_BASE_URL}${image}`;
-  else return "/src/assets/placeholders/gray-user.png";
+  else return "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
 };
