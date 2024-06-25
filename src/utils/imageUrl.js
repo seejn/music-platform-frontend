254024a2e6f930659fe936c158e22c15540c2e20
seejn <seejnmaharjan@gmail.com>
2024-06-25 23:45:39 +0545

    corrected route for the image

--- src/utils/imageUrl.js
+++ src/utils/imageUrl.js
@@ -1,5 +1,5 @@
 export const getImageUrl = (image) => {
-  if (props.user?.image) return `${import.meta.env.VITE_API_BASE_URL}${image}`;
+  if (image) return `${import.meta.env.VITE_API_BASE_URL}${image}`;
   else return "https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg";
 };
 
